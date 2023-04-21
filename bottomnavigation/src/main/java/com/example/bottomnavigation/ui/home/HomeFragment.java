e3b87836afe743ec9d53941cefd20e522c07d34b
rentschlerm <102149793+rentschlerm@users.noreply.github.com>
2023-04-21 13:33:08 +0800

    Intent(BottomNavigation)

---
+++ bottomnavigation/src/main/java/com/example/bottomnavigation/ui/home/HomeFragment.java
@@ -0,0 +1,51 @@
+package com.example.bottomnavigation.ui.home;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.bottomnavigation.SecondActivity;
+import com.example.bottomnavigation.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final Button buttonGoToSecond = binding.button;
+
+        buttonGoToSecond.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getContext(), SecondActivity.class);
+                intent.putExtra("PASS_ME", "HI! IM FROM MAIN ACTIVITY!!!");
+                startActivity(intent);
+            }
+
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
